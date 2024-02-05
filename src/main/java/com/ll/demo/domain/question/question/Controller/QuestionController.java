cf47ea63b778e26cc8f721943a6e4a88b7efdb30
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-02-05 15:42:17 +0900

    질문 리스트 , 질문 등록 버튼 추가

--- src/main/java/com/ll/demo/domain/question/question/Controller/QuestionController.java
+++ src/main/java/com/ll/demo/domain/question/question/Controller/QuestionController.java
@@ -1,13 +1,18 @@
 package com.ll.demo.domain.question.question.Controller;
 
+import com.ll.demo.domain.question.question.Entity.Question;
+import com.ll.demo.domain.question.question.Repository.QuestionRepository;
 import com.ll.demo.domain.question.question.Service.QuestionService;
 import com.ll.demo.domain.question.question.form.QuestionCreateForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,10 +20,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final QuestionRepository questionRepository;
     @GetMapping("/write")
-    public String showWrite(QuestionCreateForm questionCreateForm) {
+    public String showWrite(QuestionCreateForm questionCreateForm , Model model) {
 
-
+        model.addAttribute("questionCreateForm" , questionCreateForm);
         return "domain/question/question/question_form";
 
     }
@@ -30,5 +36,15 @@ public class QuestionController {
 
 
         return "redirect:/question/list";
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model ) {
+
+        List<Question> questions =  questionRepository.findAll();
+
+        model.addAttribute("questions" , questions);
+
+        return "domain/question/question/list";
     }
 }
